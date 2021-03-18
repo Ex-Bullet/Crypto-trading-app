f53cdb49b0f4efa5d5451dc3a9729f7cffef3ad9
Eyyup <huakq@protonmail.com>
2021-03-18 14:21:06 +0100

    Navbar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SidebarContainer = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+        </SidebarContainer>
+    );
+};
+
+export default SidebarContainer;
