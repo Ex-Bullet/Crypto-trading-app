01df876b1aa72edd139f2b779c33bc4a5e743449
Ex-Bullet <huakq@protonmail.com>
2021-04-05 01:44:13 +0200

    add data image text

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Wallet",
+  headline: "Unlimited transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: true,
+  img: require("../../images/payment.svg"),
+  alt: "Premium wallet",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,25 @@
-import React, {useState} from 'react'
-import Sidebar from '../components/Sidebar'
-import Navbar from '../components/Navbar'
-import HeroSection from '../components/HeroSection'
-import InfoSection from '../components/InfoSection'
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import { homeObjOne } from "../components/InfoSection/Data";
+
 const Home = () => {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => {
-        setIsOpen(!isOpen)
-    }
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+    </>
+  );
+};
 
-    return (
-        <>
-        <Sidebar isOpen={isOpen} toggle={toggle} />
-        <Navbar toggle={toggle}/>
-        <HeroSection />
-        <InfoSection />
-        </>
-    )
-}
-
-export default Home
+export default Home;
